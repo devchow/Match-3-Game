0aeb92aea59aefda412f03479476c63940eadfc4
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-13 17:13:45 +0300

    commit x
    commit x

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public enum PieceType
+    {
+        NORAMAL,
+        COUNT,
+    };
+
+    public struct piecePrefab
+    {
+
+    };
+
+    [Header("Dimentions")]
+    public int xDim;
+    public int yDim;
+
+    private Dictionary<PieceType, GameObject> piecePrefabDict;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
