a2a7cb8bd6f4b2d16225c3ac61017337367814ae
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-13 17:47:28 +0300

    commit x
    commit x

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -6,28 +6,73 @@ public class GridController : MonoBehaviour
 {
     public enum PieceType
     {
-        NORAMAL,
+        NORMAL,
         COUNT,
     };
 
-    public struct piecePrefab
+    [System.Serializable]
+    public struct PiecePrefab
     {
-
+        public PieceType type;
+        public GameObject prefab;
     };
 
     [Header("Dimentions")]
     public int xDim;
     public int yDim;
 
+    [Header("Array of Structs")]
+    public PiecePrefab[] piecePrefabs;
+
+    [Header("Background Prefab")]
+    public GameObject backgroundPrefab;
+
     private Dictionary<PieceType, GameObject> piecePrefabDict;
 
-    void Start()
+    // 2D Array of GameObjects
+    private GameObject[,] pieces;
+    
+    private void Start()
     {
-        
+        piecePrefabDict = new Dictionary<PieceType, GameObject>();
+
+        for(int i = 0; i < piecePrefabs.Length; i++)
+        {
+            if(!piecePrefabDict.ContainsKey(piecePrefabs[i].type))
+            {
+                piecePrefabDict.Add(piecePrefabs[i].type, piecePrefabs[i].prefab);
+            }
+        }
+
+        // Instantiation
+        for(int x = 0; x < xDim; x++)
+        {
+            for(int y = 0; y < yDim; y++)
+            {
+                GameObject background = (GameObject)Instantiate(backgroundPrefab, GetWorldPos(x, y), Quaternion.identity);
+                background.transform.parent = transform;
+            }
+        }
+
+        pieces = new GameObject[xDim, yDim];
+        for(int x = 0; x < xDim; x++)
+        {
+            for(int y = 0; y < yDim; y++)
+            {
+                pieces[x, y] = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], GetWorldPos(x, y), Quaternion.identity);
+                // Changing Name
+                pieces[x, y].name = "Piece(" + x + "," + y + ")";
+                pieces[x, y].transform.parent = transform;
+            }
+        }
     }
 
-    void Update()
+    // Update
+
+    Vector2 GetWorldPos(int x, int y)
     {
-        
+        return new Vector2(transform.position.x - xDim/2.0f + x,
+        transform.position.y + yDim / 2.0f - y);
     }
+
 }
